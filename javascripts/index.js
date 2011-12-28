d855e6fb54689b7cc1f0a4a48e9768f6817f0829
Mojo Media Labs <sean@teamrgc.com>
2011-12-28 15:44:34 -0600

    Layout in place.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,7 @@
 function getWindowHeight() {
 	var windowHeight = 0;
 
-	if (typeof(window.innerHeight) == "number") {
+	if (typeof(window.innerHeight) === "number") {
 		windowHeight = window.innerHeight;
 	} else {
 		if (document.documentElement && document.documentElement.clientHeight) {
@@ -15,3 +15,62 @@ function getWindowHeight() {
 
 	return windowHeight;
 }
+
+function setContentPosition() {
+	if (document.getElementById) {
+		var windowHeight = getWindowHeight();
+
+		if (windowHeight > 0) {
+			var contentElement = document.getElementById("wrapper");
+			var contentHeight = contentElement.offsetHeight;
+
+			if (windowHeight - contentHeight > 0) {
+				contentElement.style.position = "relative";
+				contentElement.style.top = Math.floor(((windowHeight / 2) - (contentHeight / 2))) + "px";
+			} else {
+				contentElement.style.position = "static";
+				contentElement.style.bottom = "10px";
+			}
+		}
+	}
+}
+
+window.onresize = function() {
+	setContentPosition();
+};
+
+$(function() {
+	var popupConfig = {
+		height: 600,
+		width: 850,
+		modal: true,
+		resizable: false,
+		show: "fade"
+	};
+
+	$("#wrapper").fadeIn(1000);
+	setContentPosition();
+
+	$("#footer-nav-subjects").click(function(event) {
+		var popup = $("#subjects");
+
+		popup.dialog(popupConfig);
+		$(".ui-tabs", popup).tabs();
+		event.preventDefault();
+	});
+
+	$("#footer-nav-our-dna").click(function(event) {
+		var popup = $("#our-dna");
+
+		popup.dialog(popupConfig);
+		$(".ui-tabs", popup).tabs();
+		event.preventDefault();
+	});
+
+	$("#footer-nav-scientists").click(function(event) {
+		var popup = $("#scientists");
+
+		popup.dialog(popupConfig);
+		event.preventDefault();
+	});
+});
